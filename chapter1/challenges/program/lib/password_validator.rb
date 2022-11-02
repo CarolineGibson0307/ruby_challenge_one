48f01788d0210ba6006683ccd301d05d7bb07155
Caroline <carolinegibson92@outlook.com>
2022-11-02 15:06:40 +0000

    Ready to be reviewed

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -3,4 +3,9 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
+    if password.length > 7 && password.match?(/[!@$%&]/)
+        return true
+    else  
+        return false
+    end  
 end
